6de038b757c989e611090e06d87c33baf19f2b69
Kate Bilous <kbilous@slice.com>
2017-09-27 12:23:59 +0300

    added Data_Collection

---
+++ Metrics_Grafana/Data_Collection.py
@@ -0,0 +1,7 @@
+import Metric_Generator
+
+
+metric_path = MetricGenerator(app_name ="IDF", host_name ="Clustar_Name", claster_name ="Process_Name",
+                                               process_name = 546745, step_id = 5698, process_id= "125.32.5.5")
+
+print metric_path
--- Metrics_Grafana/Metric_Generator.py
+++ Metrics_Grafana/Metric_Generator.py
@@ -7,14 +7,9 @@ import datadog
 # client = boto3.client('redshift')
 # redshift = boto3.resource('redshift')
 # redshift
-
+import Data_Collection
 
 class MetricGenerator():
-
-
-
-
-
 
 
     def __init__(self, app_name, host_name, claster_name, process_name, step_id, process_id):
@@ -40,19 +35,6 @@ class MetricGenerator():
         self._host_name
         print MetricGenerator.getMetricsValue()
 
-        # metics_path = MetricGenerator(_app_name="IDF",
-        #
-        #                               _claster_name="Clustar_Name",
-        #                               _process_name="Process_Name",
-        #                               _step_id=546745,
-        #                               _process_id=5698,
-        #                               _host_name="125.32.5.5")
 
-# metric_path = MetricGenerator(app_name = "IDF",  host_name = "Clustar_Name", claster_name = "Process_Name",
-#                               process_name = 546745, step_id = 5698, process_id= "125.32.5.5")
+print 'metics_path', Data_Collection.metric_path
 
-metric_path = MetricGenerator()
-
-metric_path_ = metric_path.setMerticsAtributes(app_name = "kate")
-
-print 'metics_path', metric_path
