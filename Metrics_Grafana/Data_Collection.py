accaaa8f5c76afc5ab48c4a60dd1495cf5a7c261
Kate Bilous <kbilous@slice.com>
2017-09-27 19:07:14 +0300

    added setpath

--- Metrics_Grafana/Data_Collection.py
+++ Metrics_Grafana/Data_Collection.py
@@ -1,7 +1,27 @@
+from  Metric_Generator import MetricGenerator
 import Metric_Generator
 
 
-metric_path = MetricGenerator(app_name ="IDF", host_name ="Clustar_Name", claster_name ="Process_Name",
-                                               process_name = 546745, step_id = 5698, process_id= "125.32.5.5")
+def setMetricPath():
+    _metric_path = MetricGenerator()
+    _metric_path.host_name = "Grafana"
+    _metric_path.step_id = 125
+    _metric_path.process_name = 'Some name'
+    _metric_path.app_name = 'Some app name'
+    _metric_path.claster_name = 'Claster name'
+    _metric_path.process_id = 456
 
-print metric_path
+    return _metric_path.process_id, _metric_path.claster_name, _metric_path.app_name, _metric_path.process_name, _metric_path.step_id,
+    _metric_path.host_name
+
+
+# jj = MetricGenerator("IDF", "Redshift", "Claster1", "Process1", 65, 85)
+
+# def objectRedefinition():
+#     metric_path = str(_metric_path)
+#     return metric_path
+
+
+print setMetricPath()
+
+print hasattr("_metric_path", "_metric_path.host_name")
--- Metrics_Grafana/Metric_Generator.py
+++ Metrics_Grafana/Metric_Generator.py
@@ -7,12 +7,18 @@ import datadog
 # client = boto3.client('redshift')
 # redshift = boto3.resource('redshift')
 # redshift
-import Data_Collection
+#app_name, host_name, claster_name, process_name, step_id, process_id
 
 class MetricGenerator():
+    def __init__(self):
+        self.app_name = ()
+        self.claster_name = ()
+        self.process_name = ()
+        self.step_id = ()
+        self.process_id = ()
+        self.host_name = ()
 
-
-    def __init__(self, app_name, host_name, claster_name, process_name, step_id, process_id):
+    def setMerticsAtributes(self, app_name, claster_name, process_name, step_id, process_id, host_name):
         self._app_name = app_name
         self._claster_name = claster_name
         self._process_name = process_name
@@ -20,21 +26,17 @@ class MetricGenerator():
         self._process_id = process_id
         self._host_name = host_name
 
-
-
-    def setMerticsAtributes(self, app_name, claster_name, process_name, step_id, process_id,host_name   ):
-        self._app_name = app_name
-        self._claster_name = claster_name
-        self._process_name = process_name
-        self._step_id = step_id
-        self._process_id = process_id
-        self._host_name = host_name
-
-    def getMetricsValue(self  ):
+    def getMetricsValue(self):
         return self._app_name, self._claster_name, self._process_name, self._step_id, self._process_id, self._process_id,
         self._host_name
         print MetricGenerator.getMetricsValue()
 
 
-print 'metics_path', Data_Collection.metric_path
+
+print hasattr('_metric_path1', 'app_name')
+
+# metric_path = {'app_name': None, 'host_name': None, 'claster_name': None, 'process_name': None, 'step_id': None,
+#                'process_id': None}
+#
+# #print metric_path
 
